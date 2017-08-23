17126fa4e1da4df2235192f51105b27a707bc1bc
smoockpp <smoockpp@gmail.com>
2017-08-23 22:58:38 +0100

    test app FB

--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     $.ajaxSetup({ cache: true });
     $.getScript('//connect.facebook.net/en_US/sdk.js', function () {
         FB.init({
-            appId: '503457173353795',
+            appId: '354535741642547',
             xfbml: true,
             version: 'v2.10'
         });
@@ -13,7 +13,7 @@ $(document).ready(function () {
         FB.AppEvents.logPageView();
         // $('#loginbutton,#feedbutton').removeAttr('disabled');
         // FB.getLoginStatus(updateStatusCallback);
-        FB.api('/503457173353795/feed', { access_token: '503457173353795|d13bc5fd011232eeaa4b5da6b720a2e4', message: 'something' }, function (response) {
+        FB.api('/354535741642547/feed', { access_token: '354535741642547|10817c3396131ec16de01399e168c085', message: 'something' }, function (response) {
             if (!response || response.error) {
                 console.error('Error occured:' + JSON.stringify(response.error));
             } else {
