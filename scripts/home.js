932c6c372c7447dbbec69adbcfa5e4f9ab3d57c2
smoockpp <smoockpp@gmail.com>
2017-08-23 21:46:39 +0100

    Adding FB integration

--- scripts/home.js
+++ scripts/home.js
@@ -2,19 +2,21 @@ $(document).ready(function() {
     $.ajaxSetup({ cache: true });
     $.getScript('//connect.facebook.net/en_US/sdk.js', function(){
         FB.init({
-            appId: '{503457173353795}',
-            autoLogAppEvents : true,
-            status           : true,
-            xfbml            : true,
-            version: 'v2.7' // or v2.1, v2.2, v2.3, ...
+            appId      : '503457173353795',
+            xfbml      : true,
+            version    : 'v2.10'
         });
+        FB.getLoginStatus(function(response) {
+            statusChangeCallback(response);
+        });
+        FB.AppEvents.logPageView();
         // $('#loginbutton,#feedbutton').removeAttr('disabled');
         // FB.getLoginStatus(updateStatusCallback);
-        FB.api('/503457173353795/feed', {access_token: '503457173353795|d13bc5fd011232eeaa4b5da6b720a2e4' ,message: 'something'}, function(response) {
+        FB.api('/503457173353795', {access_token: '503457173353795|d13bc5fd011232eeaa4b5da6b720a2e4' ,message: 'something'}, function(response) {
             if (!response || response.error) {
               console.error('Error occured:' + JSON.stringify(response.error));
             } else {
-              console.log('Post ID: ' + response.id);
+              console.log(response);
             }
         });
     });
